310ee39fc1ab49fbfa23ec92e03951d7437a179b
Marius Bongarts <mb709027@fh-muenster.de>
2019-07-19 12:13:13 +0200

    Add gradient to landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,48 +35,17 @@ export class AppRoot extends LitElement {
     const path = router.getPath();
     switch (path.toLowerCase()) {
       case 'home':
-        return html`<landing-page></landing-page>`;
+        return html`<landing-page .navItems=${this.navItems} .searchArray=${this.components}></landing-page>`;
       case 'components':
-        return html`<components-page></components-page>`;
+        return html`<components-page .navItems=${this.navItems} ></components-page>`;
       default:
-        return html`<landing-page></landing-page>`;
+        return html`<landing-page .navItems=${this.navItems} .searchArray=${this.components}></landing-page>`;
     }
   }
 
   render() {
     return html`
-    <bronco-template .hideNavForever=${this.path != 'components'.toLowerCase()}>
-          <div slot="header">
-            <bronco-top-navbar .navItems=${this.navItems} .hideOnNotTop=${false}
-            @selected=${(e: CustomEvent) => this.navItems.find(item => item === e.detail) ? router.navigate(e.detail.toLowerCase()) : ''}>
-
-              <img src="${logoSvg}" slot="left" tabindex="0" @click=${()=> router.navigate('home')}>
-
-              <bronco-searchbar
-          .searchArray=${this.components}
-          @selected=${(e: CustomEvent) => router.navigate('components?component=' + e.detail)} slot="center"></bronco-searchbar>
-
-
-              <a slot="right" href="https://github.com/marius2502/bronconents" target="_blank"><svg class="octicon octicon-mark-github v-align-middle"
-                  height="100%" viewBox="0 0 16 16" version="1.1" width="32" aria-hidden="true">
-                  <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
-                </svg></a>
-
-            </bronco-top-navbar>
-          </div>
-
-          <div slot="nav">
-            <bronco-side-navbar .hideOnMobile=${false} .navItems=${this.components} @selected=${(e:
-              CustomEvent)=>
-              router.navigate('components?component=' + e.detail)}}>
-            </bronco-side-navbar>
-          </div>
-
-          <div slot="main">
-          ${this.renderRouterOutlet()}
-          </div>
-        </bronco-template>
-
+        ${this.renderRouterOutlet()}
 `;
   }
 
--- src/components/components.page.ts
+++ src/components/components.page.ts
@@ -15,7 +15,10 @@ export class ComponentsPage extends LitElement {
   static styles = css`${unsafeCSS(componentCSS)}`;
 
   @property()
-  navItems = ['bronco-button', 'bronco-corner-navbar', 'bronco-left-navbar', 'bronco-searchbar', 'bronco-side-navbar', 'bronco-template', 'bronco-top-navbar'];
+  components = ['bronco-button', 'bronco-corner-navbar', 'bronco-left-navbar', 'bronco-searchbar', 'bronco-side-navbar', 'bronco-template', 'bronco-top-navbar'];
+
+  @property()
+  navItems!: string[];
 
   @property()
   selectedItem: string | any;
@@ -31,12 +34,6 @@ export class ComponentsPage extends LitElement {
 
     param && param.key == 'component' ? this.selectedItem = param.value : '';
 
-    // this.sectionElements.forEach(e => {
-    //   (e.offsetTop - 200) < document.documentElement.scrollTop ? this.selectedItem = e.getAttribute('id') : '';
-    // });
-    console.log(this.selectedItem);
-
-
     router.subscribe(route => {
       let param = router.getParams();
       console.log(param);
@@ -44,10 +41,17 @@ export class ComponentsPage extends LitElement {
       route === 'components'.toLowerCase() ? this.scrollToSection(this.selectedItem) : '';
     });
 
-    setTimeout(() => {
-      this.scrollToSection(this.selectedItem);
-    }, 100);
+    // window.addEventListener('scroll', () => {
+    //   this.sectionElements.forEach(e => {
+    //     (e.offsetTop - 200) < document.documentElement.scrollTop ? this.selectedItem = e.getAttribute('id') : '';
+    //   });
+    // });
 
+    if (router.getPath() === 'components') {
+      setTimeout(() => {
+        router.getPath() === 'components' ? this.scrollToSection(this.selectedItem) : '';
+      }, 100);
+    }
   }
 
   scrollToSection(item: string) {
@@ -65,33 +69,63 @@ export class ComponentsPage extends LitElement {
 
   render() {
     return html`
-      <section id='bronco-button'>
-        <button-overview></button-overview>
-      </section>
+        <bronco-template .hideNavForever=${false}>
+          <div slot="header">
+            <bronco-top-navbar .navItems=${this.navItems} .hideOnNotTop=${false} @selected=${(e: CustomEvent) =>
+        this.navItems.find(item => item === e.detail) ? router.navigate(e.detail.toLowerCase()) : ''}>
 
-      <section id='bronco-corner-navbar'>
-        <corner-navbar-overview></corner-navbar-overview>
-      </section>
+              <img src="${logoSvg}" slot="left" tabindex="0" @click=${()=> router.navigate('home')}>
 
-      <section id='bronco-left-navbar'>
-        <left-navbar-overview></left-navbar-overview>
-      </section>
+              <bronco-searchbar .searchArray=${this.components} @selected=${(e: CustomEvent) =>
+        router.navigate('components?component=' + e.detail)} slot="center"></bronco-searchbar>
 
-      <section id='bronco-searchbar'>
-        <searchbar-overview></searchbar-overview>
-      </section>
 
-      <section id='bronco-side-navbar'>
-        <side-navbar-overview></side-navbar-overview>
-      </section>
+              <a slot="right" href="https://github.com/marius2502/bronconents" target="_blank"><svg class="octicon octicon-mark-github v-align-middle"
+                  height="100%" viewBox="0 0 16 16" version="1.1" width="32" aria-hidden="true">
+                  <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                </svg></a>
 
-      <section id='bronco-template'>
-        <template-overview></template-overview>
-      </section>
+            </bronco-top-navbar>
+          </div>
 
-      <section id='bronco-top-navbar'>
-        <top-navbar-overview></top-navbar-overview>
-      </section>
+          <div slot="nav">
+            <bronco-side-navbar selectedItem=${this.selectedItem} .hideOnMobile=${false} .navItems=${this.components} @selected=${(e:
+                    CustomEvent)=>
+                router.navigate('components?component=' + e.detail)}}>
+            </bronco-side-navbar>
+          </div>
+
+          <div slot="main">
+            <section id='bronco-button'>
+              <button-overview></button-overview>
+            </section>
+
+            <section id='bronco-corner-navbar'>
+              <corner-navbar-overview></corner-navbar-overview>
+            </section>
+
+            <section id='bronco-left-navbar'>
+              <left-navbar-overview></left-navbar-overview>
+            </section>
+
+            <section id='bronco-searchbar'>
+              <searchbar-overview></searchbar-overview>
+            </section>
+
+            <section id='bronco-side-navbar'>
+              <side-navbar-overview></side-navbar-overview>
+            </section>
+
+            <section id='bronco-template'>
+              <template-overview></template-overview>
+            </section>
+
+            <section id='bronco-top-navbar'>
+              <top-navbar-overview></top-navbar-overview>
+            </section>
+          </div>
+        </bronco-template>
+
 `;
   }
 
